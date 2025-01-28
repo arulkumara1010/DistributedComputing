b3f128b36457885dccb8463cbd797bcff6add590
Arulkumara Babu <22z310@psgtech.ac.in>
2025-01-28 11:22:15 +0530

    formatting

--- Deadlock/cent.c
+++ Deadlock/cent.c
@@ -3,76 +3,79 @@
 #define n 5
 int dfs(int process, int adj[n][n], int visited[n], int stack[n])
 {
-visited[process] = 1;
-stack[process] = 1;
-for (int i = 0; i < n; i++)
-{
-if (adj[process][i] == 1)
-{
-if (!visited[i] && dfs(i, adj, visited, stack))
-{
-return 1;
-}
-else if (stack[i])
-{
-return 1;
-}
-}
-}
-stack[process] = 0;
-return 0;
+    visited[process] = 1;
+    stack[process] = 1;
+    for (int i = 0; i < n; i++)
+    {
+        if (adj[process][i] == 1)
+        {
+            if (!visited[i] && dfs(i, adj, visited, stack))
+            {
+                return 1;
+            }
+            else if (stack[i])
+            {
+                return 1;
+            }
+        }
+    }
+    stack[process] = 0;
+    return 0;
 }
 int cycledetect(int adj[n][n])
 {
-int visited[n] = {0};
-int stack[n] = {0};
-for (int i = 0; i < n; i++)
+    int visited[n] = {0};
+    int stack[n] = {0};
+    for (int i = 0; i < n; i++)
+    {
+        if (!visited[i])
+        {
+            if (dfs(i, adj, visited, stack))
+            {
+                return 1;
+            }
+        }
+    }
+    return 0;
+}
+int main()
 {
-if (!visited[i])
-{
-if (dfs(i, adj, visited, stack))
-{
-return 1;
-}
-}
-}
-return 0;
-}
-int main(){
-int adj[n][n]={0};
-printf("Add dependecy between processes\n");
-printf("Enter -1 -1 to stop\n");
-while(1)
-{
-int from,to;
-scanf("%d %d",&from,&to);
-if(from==-1 && to==-1)
-{
-break;
-}
-if(from >=0 && from<n && to>=0 && to<n)
-{
-adj[from][to]=1;
-printf("Process %d->%d added\n",from,to);
-}
-else
-{
-printf("Invalid input\n");
-}
-}
-printf("Adjacency matrix\n");
-for(int i=0; i<n; i++){
-for(int j=0; j<n; j++){
-printf("%d",adj[i][j]);
-}
-printf("\n");
-}
-if(cycledetect(adj))
-{
-printf("Deadlock detected\n");
-}
-else
-{
-printf("No deadlock detected\n");
-}
+    int adj[n][n] = {0};
+    printf("Add dependecy between processes\n");
+    printf("Enter -1 -1 to stop\n");
+    while (1)
+    {
+        int from, to;
+        scanf("%d %d", &from, &to);
+        if (from == -1 && to == -1)
+        {
+            break;
+        }
+        if (from >= 0 && from < n && to >= 0 && to < n)
+        {
+            adj[from][to] = 1;
+            printf("Process %d->%d added\n", from, to);
+        }
+        else
+        {
+            printf("Invalid input\n");
+        }
+    }
+    printf("Adjacency matrix\n");
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            printf("%d", adj[i][j]);
+        }
+        printf("\n");
+    }
+    if (cycledetect(adj))
+    {
+        printf("Deadlock detected\n");
+    }
+    else
+    {
+        printf("No deadlock detected\n");
+    }
 }
--- Deadlock/hier.c
+++ Deadlock/hier.c
@@ -6,15 +6,21 @@
 #define MAX_PROCESSES 10
 #define MAX_REGIONS 3
 
-bool detect_cycle_dfs(int graph[MAX_PROCESSES][MAX_PROCESSES], int num_processes, int node, int visited[], int stack[]) {
+bool detect_cycle_dfs(int graph[MAX_PROCESSES][MAX_PROCESSES], int num_processes, int node, int visited[], int stack[])
+{
     visited[node] = 1;
     stack[node] = 1;
 
-    for (int i = 0; i < num_processes; i++) {
-        if (graph[node][i]) {
-            if (!visited[i] && detect_cycle_dfs(graph, num_processes, i, visited, stack)) {
+    for (int i = 0; i < num_processes; i++)
+    {
+        if (graph[node][i])
+        {
+            if (!visited[i] && detect_cycle_dfs(graph, num_processes, i, visited, stack))
+            {
                 return true;
-            } else if (stack[i]) {
+            }
+            else if (stack[i])
+            {
                 return true;
             }
         }
@@ -24,12 +30,15 @@ bool detect_cycle_dfs(int graph[MAX_PROCESSES][MAX_PROCESSES], int num_processes
     return false;
 }
 
-bool detect_deadlock(int graph[MAX_PROCESSES][MAX_PROCESSES], int num_processes) {
+bool detect_deadlock(int graph[MAX_PROCESSES][MAX_PROCESSES], int num_processes)
+{
     int visited[MAX_PROCESSES] = {0};
     int stack[MAX_PROCESSES] = {0};
 
-    for (int i = 0; i < num_processes; i++) {
-        if (!visited[i] && detect_cycle_dfs(graph, num_processes, i, visited, stack)) {
+    for (int i = 0; i < num_processes; i++)
+    {
+        if (!visited[i] && detect_cycle_dfs(graph, num_processes, i, visited, stack))
+        {
             return true;
         }
     }
@@ -37,37 +46,49 @@ bool detect_deadlock(int graph[MAX_PROCESSES][MAX_PROCESSES], int num_processes)
     return false;
 }
 
-void hierarchical_deadlock_detection(int regions[MAX_REGIONS][MAX_PROCESSES][MAX_PROCESSES], int num_processes[], int num_regions) {
+void hierarchical_deadlock_detection(int regions[MAX_REGIONS][MAX_PROCESSES][MAX_PROCESSES], int num_processes[], int num_regions)
+{
     int global_wfg[MAX_PROCESSES][MAX_PROCESSES] = {0};
 
     printf("----- Hierarchical Deadlock Detection -----\n");
 
-    for (int i = 0; i < num_regions; i++) {
+    for (int i = 0; i < num_regions; i++)
+    {
         printf("Checking for deadlocks in Region %d...\n", i);
-        if (detect_deadlock(regions[i], num_processes[i])) {
+        if (detect_deadlock(regions[i], num_processes[i]))
+        {
             printf("Deadlock detected in Region %d! Escalating unresolved graph to global coordinator...\n", i);
 
-            for (int u = 0; u < num_processes[i]; u++) {
-                for (int v = 0; v < num_processes[i]; v++) {
-                    if (regions[i][u][v]) {
+            for (int u = 0; u < num_processes[i]; u++)
+            {
+                for (int v = 0; v < num_processes[i]; v++)
+                {
+                    if (regions[i][u][v])
+                    {
                         global_wfg[u][v] = 1;
                     }
                 }
             }
-        } else {
+        }
+        else
+        {
             printf("No deadlocks in Region %d.\n", i);
         }
     }
 
     printf("Checking global WFG for system-wide deadlocks...\n");
-    if (detect_deadlock(global_wfg, MAX_PROCESSES)) {
+    if (detect_deadlock(global_wfg, MAX_PROCESSES))
+    {
         printf("System-wide deadlock detected! Taking global resolution actions...\n");
-    } else {
+    }
+    else
+    {
         printf("No system-wide deadlock detected.\n");
     }
 }
 
-int main() {
+int main()
+{
     int regions[MAX_REGIONS][MAX_PROCESSES][MAX_PROCESSES] = {{{0}}};
     int num_processes[MAX_REGIONS];
     int num_regions;
@@ -75,13 +96,16 @@ int main() {
     printf("Enter the number of regions: ");
     scanf("%d", &num_regions);
 
-    for (int i = 0; i < num_regions; i++) {
+    for (int i = 0; i < num_regions; i++)
+    {
         printf("Enter the number of processes in Region %d: ", i);
         scanf("%d", &num_processes[i]);
 
         printf("Enter the adjacency matrix for Region %d (size %dx%d):\n", i, num_processes[i], num_processes[i]);
-        for (int u = 0; u < num_processes[i]; u++) {
-            for (int v = 0; v < num_processes[i]; v++) {
+        for (int u = 0; u < num_processes[i]; u++)
+        {
+            for (int v = 0; v < num_processes[i]; v++)
+            {
                 scanf("%d", &regions[i][u][v]);
             }
         }
